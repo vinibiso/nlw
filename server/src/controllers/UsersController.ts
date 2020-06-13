efb79c987ba621a7dcfe7ba110d25a12a3ea5784
Vinicius Bisognin Immich <vinicius@Viniciuss-MacBook-Pro.local>
2020-06-13 14:15:38 -0300

    add prisma to UsersController

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
 
-import knex from '../database/connection';
 import generateWebToken from '../utils/gerenerateWebToken';
 
 class UsersController {
   async create(request: Request, response: Response) {
     const { email, password } = request.body;
 
-    const userFound = await knex('users').where('email', email);
+    const prisma = new PrismaClient();
 
-    if (userFound.length > 0) {
+    const userFound = await prisma.users.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (userFound) {
       return response.status(400).json({ message: 'User already registered'})
     }
 
@@ -21,26 +27,27 @@ class UsersController {
       password: hashedPassword,
     };
 
-    const insertedIds = await knex('users').insert(user);
-    const userId = insertedIds[0];
+    const insertedUser = await prisma.users.create({ data: user });
+    delete insertedUser.password;
 
-    const newUser = {
-      id: userId,
-      emaail: user.email,
-    };
+    const token = generateWebToken({ id: insertedUser.id });
 
-    const token = generateWebToken({ id: newUser.id });
-
-    return response.json({ user: newUser, token });
+    return response.json({ user: insertedUser, token });
   }
 
   async authenticate(request: Request, response: Response) {
     const { email, password } = request.body;
 
-    const user = await knex('users').where('email', email).first();
+    const prisma = new PrismaClient();
+
+    const user = await prisma.users.findOne({
+      where: {
+        email,
+      },
+    });
 
     if (!user) {
-      return response.status(400).json({ message: 'User not found'});
+      return response.status(400).json({ message: 'User not found' });
     }
 
     if (!await bcrypt.compare(password, user.password)) {
