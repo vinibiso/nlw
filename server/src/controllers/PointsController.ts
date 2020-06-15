7d30dc7bda886e51ea2ce952028bb8ed119e16b7
Vinicius Bisognin Immich <vinicius@Viniciuss-MacBook-Pro.local>
2020-06-15 15:44:16 -0300

    using prisma

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import prisma from '../connection';
-import knex from '../database/connection';
 
 class PointsController {
   async index(request: Request, response: Response) {
@@ -10,13 +9,19 @@ class PointsController {
       .split(',')
       .map((item) => Number(item.trim()));
 
-    const points = await knex('points')
-      .join('point_items', 'points.id', '=', 'point_items.point_id')
-      .whereIn('point_items.item_id', parsedItems)
-      .where('city', String(city))
-      .where('uf', String(uf))
-      .distinct()
-      .select('points.*');
+    const points = await prisma.point.findMany({
+      where: {
+        city: String(city),
+        uf: String(uf),
+        items: {
+          some: {
+            id: {
+              in: parsedItems,
+            },
+          },
+        },
+      },
+    });
 
     const serializedPoints = points.map((point) => {
       return {
@@ -31,13 +36,18 @@ class PointsController {
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
-    const point = await prisma.points.findOne({
+    const point = await prisma.point.findOne({
       where: {
         id: Number(id),
       },
+      include: {
+        items: {
+          select: {
+            title: true,
+          },
+        },
+      },
     });
-
-    console.log(point);
 
     if (!point) {
       return response.status(400).json({ message: 'Point not found' });
@@ -48,12 +58,7 @@ class PointsController {
       image_url: `http://192.168.0.103:3333/uploads/${point.image}`,
     };
 
-    const items = await knex('items')
-      .join('point_items', 'items.id', '=', 'point_items.item_id')
-      .where('point_items.point_id', id)
-      .select('items.title');
-
-    return response.json({ serializedPoint, items });
+    return response.json(serializedPoint);
   }
 
   async create(request: Request, response: Response) {
@@ -86,13 +91,21 @@ class PointsController {
       longitude,
       city,
       uf,
-      point_items: {
+      items: {
         connect: pointItems,
       },
     };
 
-    const insertedPoint = await prisma.points.create({
+    const insertedPoint = await prisma.point.create({
       data: point,
+      include: {
+        items: {
+          select: {
+            id: true,
+            title: true,
+          },
+        },
+      },
     });
 
     return response.json(insertedPoint);
